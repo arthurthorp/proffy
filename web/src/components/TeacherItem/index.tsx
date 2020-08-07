b9519dd692764ba4ff020f145a282a93932aba54
Arthur Tavares <arthurthorp@hotmail.com>
2020-08-07 17:56:01 -0300

    Finalização do site para web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,35 +2,55 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 
-function TeacherItem (){
+
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://lh3.googleusercontent.com/ogw/ADGmqu-iOa4zqlJ6P9kGEl3XT8mi386ZyVkyWuElvbhv=s32-c-mo" alt="Arthur Tavares"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Arthur Tavares</strong>
-                    <span>Física</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                <br/><br/>
-                Ipsum at dignissimos vero eius. Incidunt asperiores pariatur blanditiis! Maiores distinctio possimus, ipsa eaque saepe animi fugit, nesciunt impedit ea quidem similique.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" rel="noopener noreferrer" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
 
