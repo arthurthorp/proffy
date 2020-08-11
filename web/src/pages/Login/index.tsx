1f4359274713c7041185fbe58044c4efa0162fd6
Arthur Tavares <arthurthorp@hotmail.com>
2020-08-10 21:40:52 -0300

    Desenvolvimento do layout da página de login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 
@@ -14,6 +15,23 @@ function Login() {
                     <h2>Sua plataforma de estudos online</h2>
                 </div>
             </header>
+
+            <main className="login-main">
+                <div className="content-main">
+                    <div className="header-main">
+                        <strong className="title-main">Fazer login</strong>
+                        <Link className="register-main" to="/">
+                            Criar uma conta
+                        </Link>
+                    </div>
+
+                    <div className="form-main">
+                        <input type="email" placeholder="E-mail" className="input-login" required/>
+                        <input type="password" placeholder="Senha" className="input-login" required/>
+                    </div>
+                </div>
+
+            </main>
         </div>
     );
 }
